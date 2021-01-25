43321dfcb34579b658e58661bff5d46cd061b93c
clock-me <nickolai.palchickov@yandex.ru>
2021-01-25 16:54:51 +0300

    add more params in inference function

--- style_transfer.py
+++ style_transfer.py
@@ -11,8 +11,9 @@ def get_style_transfer(model: TransferModel,
                        sp: spm.SentencePieceProcessor,
                        preprocessed_batch: tp.List[str],
                        dest_styles: tp.List[int],
-                       temperature: float = 1.0,
+                       temperature: float = 0.001,
                        max_steps: int = 30,
+                       bos_token: int = 1,
                        eos_token: int = 2) -> tp.List[str]:
     """
     Get style transfer of batch of text
@@ -23,7 +24,7 @@ def get_style_transfer(model: TransferModel,
 
     styles = torch.tensor(dest_styles, dtype=int, device=model.encoder.embedding.weight.device)
     translated_batch, pad_mask = model.temperature_translate_batch(batch_ids, batch_ids != 0, styles,
-                                                                   temperature, max_steps, eos_token)
+                                                                   temperature, max_steps, bos_token, eos_token)
     translated_batch *= pad_mask
 
     result = [sp.decode(item) for item in translated_batch.T.tolist()]
